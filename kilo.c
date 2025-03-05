eb9af3b91c8e3fcf4201991014aa9df8ca60b959
Kade <kadepeay@gmail.com>
2025-03-04 21:22:30 -0700

    Give default message for status bar

--- kilo.c
+++ kilo.c
@@ -7,11 +7,13 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h> // Terminal input output control
 #include <sys/types.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 /*** defines ***/
@@ -54,6 +56,8 @@ struct editorConfig
     int numrows;
     erow *row;
     char *filename;
+    char statusmsg[80];
+    time_t statusmsg_time;
     struct termios orig_termios;
 };
 
@@ -444,6 +448,18 @@ void editorDrawStatusBar(struct abuf *ab) {
         }
     }
     abAppend(ab, "\x1b[m", 3);
+    abAppend(ab, "\r\n", 2);
+}
+
+void editorDrawMessageBar(struct abuf *ab) {
+    abAppend(ab, "\x1b[K", 3);
+    int msglen = strlen(E.statusmsg);
+    if (msglen > E.screencols) {
+        msglen = E.screencols;
+    }
+    if (msglen && time(NULL) - E.statusmsg_time < 5) {
+        abAppend(ab, E.statusmsg, msglen);
+    }
 }
 
 void editorRefreshScreen()
@@ -457,6 +473,7 @@ void editorRefreshScreen()
 
     editorDrawRows(&ab); // Draw all the rows
     editorDrawStatusBar(&ab); // Draw the status bar
+    editorDrawMessageBar(&ab);
 
     // Position the cursor at correct location
     char buf[32];
@@ -468,6 +485,14 @@ void editorRefreshScreen()
     // Write the buffer to terminal
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab); // Free the buffer
+}
+
+void editorSetStatusMessage(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+    va_end(ap);
+    E.statusmsg_time = time(NULL);
 }
 
 /*** input ***/
@@ -580,12 +605,14 @@ void initEditor()
     E.numrows = 0;
     E.row = NULL;
     E.filename = NULL;
+    E.statusmsg[0] = '\0';
+    E.statusmsg_time = 0;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     {
         die("getWindowSize");
     }
-    E.screenrows -= 1;
+    E.screenrows -= 2;
 }
 
 int main(int argc, char *argv[])
@@ -595,6 +622,9 @@ int main(int argc, char *argv[])
     if (argc >= 2) {
         editorOpen(argv[1]);
     }
+    
+    // set initial status message
+    editorSetStatusMessage("HELP: Ctrl-Q = quit");
 
     while (1)
     {
